2a012d9c9a3a3d0c46de769018ab3f474d5da984
bim <srgprofile@yandex.ru>
2026-03-25 19:35:09 +0300

    Добавил help команду поиска!

--- lab1/cli/bin/cli.dart
+++ lab1/cli/bin/cli.dart
@@ -4,13 +4,16 @@ const version = '0.0.1';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); // Change this from 'Hello, Dart!'
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    print('Search command recognized!');
   } else {
-    printUsage(); // Catch-all for any unrecognized command.
+    printUsage();
   }
 }
+
 
 
 void printUsage() { // Add this new function
