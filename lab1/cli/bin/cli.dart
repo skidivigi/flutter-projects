388b7f2ecf2c241952fd3fd75ff52503f12e50fe
bim <srgprofile@yandex.ru>
2026-03-25 21:02:17 +0300

    Добавил логирование результатов поиска + игнор лог директории

--- lab1/cli/bin/cli.dart
+++ lab1/cli/bin/cli.dart
@@ -1,25 +1,35 @@
 // import 'package:cli/cli.dart' as cli;
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:cli/cli.dart';
 import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) {
-  var commandRunner = CommandRunner(
-    onOutput: (String output) async {
-      await write(output);
-    },
-    onError: (Object error) {
-      if (error is Error) {
-        throw error;
-      }
-      if (error is Exception) {
-        print(error);
-      }
-    },
-  )..addCommand(HelpCommand());
-  commandRunner.run(arguments);
+void main(List<String> arguments) async {
+  final errorLogger = initFileLogger('errors');
+  final app =
+      CommandRunner(
+          onOutput: (String output) async {
+            await write(output);
+          },
+          onError: (Object error) {
+            if (error is Error) {
+              errorLogger.severe(
+                '[Error] ${error.toString()}\n${error.stackTrace}',
+              );
+              throw error;
+            }
+            if (error is Exception) {
+              errorLogger.warning(error);
+            }
+          },
+        )
+        ..addCommand(HelpCommand())
+        ..addCommand(SearchCommand(logger: errorLogger))
+        ..addCommand(GetArticleCommand(logger: errorLogger));
+
+  app.run(arguments);
 }
 
 // Future<String> getWikipediaArticle(String articleTitle) async {
