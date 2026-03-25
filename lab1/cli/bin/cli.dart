5d0c32629925e55c1609eea56ffabe8bd25ed649
bim <srgprofile@yandex.ru>
2026-03-25 19:37:18 +0300

    Добавил функцию поиска

--- lab1/cli/bin/cli.dart
+++ lab1/cli/bin/cli.dart
@@ -8,15 +8,19 @@ void main(List<String> arguments) {
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
-    print('Search command recognized!');
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
     printUsage();
   }
 }
 
 
+void searchWikipedia(List<String>? arguments) {
+  print('searchWikipedia received arguments: $arguments');
+}
 
-void printUsage() { // Add this new function
+void printUsage() {
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
   );
