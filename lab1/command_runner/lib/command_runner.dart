e7ade11642ee0955057c56ba99d43e7d90ada3ac
bim <srgprofile@yandex.ru>
2026-03-25 19:48:58 +0300

    Добавление класса

--- lab1/command_runner/lib/command_runner.dart
+++ lab1/command_runner/lib/command_runner.dart
@@ -1,5 +1,4 @@
 class CommandRunner {
-  /// Runs the command-line application logic with the given arguments.
   Future<void> run(List<String> input) async {
     print('CommandRunner received arguments: $input');
   }
